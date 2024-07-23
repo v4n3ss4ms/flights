702ff1fc19baafb543fb5a9739ab3a8ec4a5d8df
v4n3ss4ms <v4n3ss4ms@gmail.com>
2024-07-23 16:59:59 +0200

    feat: toast component

---
+++ src/components/toast-msg/toast-msg.component.tsx
@@ -0,0 +1,51 @@
+import { Fragment } from 'react';
+import { IconButton } from '@mui/material';
+import { Close as CloseIcon}  from '@mui/icons-material';
+import {Snackbar} from '@mui/joy';
+
+
+export type SeverityType = 'primary' | 'neutral' | 'danger' | 'success' | 'warning';
+
+interface ToastMsgProps {
+  isOpen: boolean;
+  setIsOpen: (shouldOpenToast: boolean) => void;
+  severity: SeverityType;
+  msg: string;
+}
+
+export default function ToastMsg(props: ToastMsgProps) {
+  const { isOpen, setIsOpen, severity, msg } = props;
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const closeBtn = (
+    <Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </Fragment>
+  );
+
+  return (
+
+    <Snackbar
+      autoHideDuration={6000}
+      color={severity}
+      size="md"
+      variant="solid"
+      anchorOrigin={{ vertical:'top', horizontal:'right' }}
+      open={isOpen}
+      onClose={handleClose}
+      endDecorator={closeBtn}
+    >
+      {msg}
+    </Snackbar>
+  );
+}
